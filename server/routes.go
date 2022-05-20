9de313a8cd84e93a3c703f224b53a71714f155f8
arshamalh <arshamalh@gmail.com>
2022-05-20 16:42:13 +0430

    routes message improved.

--- server/routes.go
+++ server/routes.go
@@ -19,42 +19,58 @@ func AddTrackingUsers(ctx *fiber.Ctx) error {
 		_, err := scheduler.Every(5).Minutes().Tag(job_name).Do(TrackUser, user_id)
 		var msg string
 		if err == nil {
-			msg = fmt.Sprintf("tracking user %s started\n", user_id)
+			msg = fmt.Sprintf("tracking user %s started", user_id)
 		} else {
 			msg = fmt.Sprintf("could not start tracking user %s", user_id)
 		}
 		json_msgs = append(json_msgs, msg)
 	}
 
-	return ctx.Status(200).JSON(json_msgs)
+	return ctx.Status(200).JSON(fiber.Map{
+		"data": json_msgs,
+	})
 }
 
 func StopTrackingUsers(ctx *fiber.Ctx) error {
 	var user_ids []string
+	var json_msgs []string
 
 	if err := ctx.BodyParser(&user_ids); err != nil {
 		return err
 	}
 
 	for _, user_id := range user_ids {
+		var msg string
 		if err := scheduler.RemoveByTag(fmt.Sprintf("user_track_%s", user_id)); err == nil {
-			fmt.Printf("tracking user %s stopped\n", user_id)
+			msg = fmt.Sprintf("tracking user %s stopped", user_id)
+		} else {
+			msg = fmt.Sprintf("could not stop tracking user %s", user_id)
 		}
+		json_msgs = append(json_msgs, msg)
 	}
-	return ctx.JSON(true)
+	return ctx.Status(200).JSON(fiber.Map{
+		"data": json_msgs,
+	})
 }
 
 func StopTrackingTweets(ctx *fiber.Ctx) error {
 	var tweet_ids []string
+	var json_msgs []string
 
 	if err := ctx.BodyParser(&tweet_ids); err != nil {
 		return err
 	}
 
 	for _, tweet_id := range tweet_ids {
+		var msg string
 		if err := scheduler.RemoveByTag(fmt.Sprintf("tweet_track_%s", tweet_id)); err == nil {
-			fmt.Printf("tracking tweet %s stopped\n", tweet_id)
+			msg = fmt.Sprintf("tracking tweet %s stopped", tweet_id)
+		} else {
+			msg = fmt.Sprintf("could not stop tracking tweet %s", tweet_id)
 		}
+		json_msgs = append(json_msgs, msg)
 	}
-	return ctx.JSON(true)
+	return ctx.Status(200).JSON(fiber.Map{
+		"data": json_msgs,
+	})
 }
