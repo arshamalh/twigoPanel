04426de6cb8ef67ebb51d0a478aa098b30519e78
arshamalh <arshamalh@gmail.com>
2022-06-04 01:02:47 +0430

    user struct improved.

--- server/database/user.go
+++ server/database/user.go
@@ -4,4 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	UserTwitterID string  `json:"user_twitter_id"`
+	Username      string  `json:"username"`
+	Tweets        []Tweet `json:"tweets"`
 }
